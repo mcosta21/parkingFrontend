061d1e10f3ed6a4ef87b0d9e77e080d69c6458aa
Fernando Cerveira <cerveira9@gmail.com>
2019-10-04 21:28:22 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 import logo from './logo.svg';
 import './App.css';
+import Atendimento from './components/Atendimento';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Atendimento />
     </div>
   );
 }
---
+++ src/components/Atendimento.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Atendimento extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            atendimentos: [],
+            loading: true
+        };
+    }
+
+    UNSAFE_componentWillMount() {
+        this.getRequest();
+    }
+
+    getRequest() {
+        axios
+            .get('http://192.168.246.117:8080/estacionamento/rest/ws/getAtendimentos/')
+            .then(res =>
+                this.setState({ atendimentos: res.data, loading: false }),
+            );
+    }
+
+    renderAtendimentos() {
+        console.log('entrou no render atendimentos');
+        console.log('this.state.atendimentos', this.state.atendimentos);
+        return this.state.atendimentos.map(valor => (
+            <div>
+                <h3>{valor.cliente.nomeDoCliente}</h3>
+            </div>
+        ))
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Atendimento</h1>
+                {this.renderAtendimentos()}
+                <ProgressSpinner/>
+            </div>
+        );
+    }
+}
+
+export default Atendimento;
